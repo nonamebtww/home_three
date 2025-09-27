286ea07112b3f50ce645b8ce8e3e08752d0df185
nevermindjq <60211617+nevermindjq@users.noreply.github.com>
2025-09-27 08:20:15 +0300

    Add project files.

---
+++ home_three/main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void main() {
+	setlocale(LC_ALL, "RUS");
+
+	double U, R, I;
+
+	printf("Введите напряжение (U): ");
+	scanf_s("%lf", &U);
+
+	printf("Введите сопротивление (R): ");
+	scanf_s("%lf", &R);
+
+	I = U / R;
+
+	printf("Напряжение равно (I): %lf Вт. / %lf Ом = %lf А", U, R, I);
+}
